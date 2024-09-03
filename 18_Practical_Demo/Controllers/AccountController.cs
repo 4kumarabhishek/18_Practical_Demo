145c1dc3dc32bb835e1a62bd9f06830685831051
4kumarabhishek <4kumar.abhishek@gmail.com>
2024-09-03 18:39:35 +0530

    Add project files.

---
+++ 18_Practical_Demo/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _18_Practical_Demo.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ViewResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection values)
+        {
+            if (values["user"] == "admin" && values["pass"] == "12345")
+            {
+                Session["uname"] = "admin";
+                return RedirectToAction("Personal", "Student");
+            }
+
+            else
+            {
+                ViewBag.msg = "Invalid";
+                return View();
+            }
+                
+        }
+    }
+}
---
+++ 18_Practical_Demo/Controllers/StudentController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _18_Practical_Demo.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ViewResult Personal()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Personal(string sname, string contact, string address)
+        {
+            if(sname==""|| contact=="" || address == "")
+            {
+                ViewBag.msg = "Please enter valid details";
+                return View();
+            }
+            else
+            {
+                TempData["name"] = sname;
+                TempData["contact"] = contact;
+                TempData["address"] = address;
+
+                return RedirectToAction("Marks");
+            }
+        }
+
+        public ViewResult Marks()
+        {
+            return View();
+        }
+    }
+}
